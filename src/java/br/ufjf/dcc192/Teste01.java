349fd1b48776fba217362102f8ac9cecdfe37e87
ice <ice@lab06m38.nrc.ice.ufjf.br>
2018-03-09 22:08:25 -0300

    hello

--- src/java/br/ufjf/dcc192/Teste01.java
+++ src/java/br/ufjf/dcc192/Teste01.java
@@ -6,7 +6,9 @@
 package br.ufjf.dcc192;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,11 +17,14 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author ice
  */
+@WebServlet(name = "helloServlet",urlPatterns = "/hello.html")
 public class Teste01 extends HttpServlet {
    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Hello World");
+        PrintWriter out = resp.getWriter();
+        out.println("Hello World - cliente");
     }
     
 }
